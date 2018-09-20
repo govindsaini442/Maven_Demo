00c9f6a52ad8a3033670337de58703f9ee8d8f29
Govind <govindsaini442@gmail.com>
2018-09-20 10:43:16 +0530

    updated app.java

--- src/main/java/com/govind/maven_demo/App.java
+++ src/main/java/com/govind/maven_demo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Govind!" );
+        System.out.println( "Welcome to Maven Demo!" );
     }
 }
